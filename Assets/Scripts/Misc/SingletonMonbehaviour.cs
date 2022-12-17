6186b3fe29881afe064dc7b1d0f22f0b3b4a9aa9
irem <iremaydinn.7@gmail.com>
2022-12-17 18:38:38 +0300

    singleton

---
+++ Assets/Scripts/Misc/SingletonMonbehaviour.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SingletonMonbehaviour<T> : MonoBehaviour where T: MonoBehaviour
+{
+
+    private static T instance;
+    // static variables can be accessed using the class name
+    public static T Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this as T;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
+}

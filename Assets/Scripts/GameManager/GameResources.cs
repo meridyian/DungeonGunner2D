7c36f38f702df8c17b53111deec526e891dca638
irem <iremaydinn.7@gmail.com>
2022-12-19 22:53:46 +0300

    player script added

--- Assets/Scripts/GameManager/GameResources.cs
+++ Assets/Scripts/GameManager/GameResources.cs
@@ -31,6 +31,21 @@ public class GameResources : MonoBehaviour
 
     public RoomNodeTypeListSO roomNodeTypeList;
     
+    #region Header PLAYER
+
+    [Space(10)]
+    [Header("PLAYER")]
+
+    #endregion Header PLAYER
+
+    #region Tooltip
+
+    [Tooltip("The current player scriptable object - this is used to reference the current player between scenes")]
+
+    #endregion Tooltip
+
+    public CurrentPlayerSO CurrentPlayer;
+    
     #region Header MATERIALS
 
     [Space(10)]
---
+++ Assets/Scripts/Health/Health.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    private int startingHealth;
+    private int currentHealth;
+    
+    //set starting health
+
+    public void SetStartingHealth(int startingHealth)
+    {
+        this.startingHealth = startingHealth;
+        currentHealth = startingHealth;
+    }
+    
+    // get the starting health 
+    
+    public int GetStartingHealth()
+    {
+        return startingHealth;
+    }
+
+}

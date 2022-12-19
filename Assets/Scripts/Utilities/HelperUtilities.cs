572a8f02efd29873f084c4f8a46b9a71a799debf
irem <iremaydinn.7@gmail.com>
2022-12-19 22:18:22 +0300

    validate added

--- Assets/Scripts/Utilities/HelperUtilities.cs
+++ Assets/Scripts/Utilities/HelperUtilities.cs
@@ -19,6 +19,20 @@ public static class  HelperUtilities
         return false;
     }
     
+    // null value debug check
+
+    public static bool ValidateCheckNullValue(Object thisObject, string fieldName, UnityEngine.Object objectToCheck)
+    {
+        if (objectToCheck == null)
+        {
+            Debug.Log(fieldName + " is null and must contain a value in object " + thisObject.name.ToString());
+            return true;
+            
+        }
+
+        return false;
+    }
+    
     
     // checks if the thing you passed is empty
     
@@ -53,6 +67,33 @@ public static class  HelperUtilities
         if (count == 0)
         {
             Debug.Log(fieldName + " has no values in object" + thisObject.name.ToString());
+        }
+
+        return error;
+    }
+    
+    // positive value debug check if zero is allowed set isZeroAllowed to true, retruns true if there is an error
+    public static bool ValidateCheckPositiveValue(Object thisObject, string fieldName, int valueToCheck,
+        bool isZeroAllowed)
+    {
+        bool error = false;
+
+        if (isZeroAllowed)
+        {
+            if (valueToCheck < 0)
+            {
+                Debug.Log(fieldName + " must contain a positive value or zero in object" + thisObject.name.ToString());
+                error = true;
+                
+            }
+        }
+        else
+        {
+            if (valueToCheck <= 0)
+            {
+                Debug.Log(fieldName + " must contain a positive value in object" + thisObject.name.ToString());
+                error = true;
+            }
         }
 
         return error;

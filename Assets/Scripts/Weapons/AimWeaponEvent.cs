6224f38bb6728a9c64039f81d476459c2c5319c1
irem <iremaydinn.7@gmail.com>
2022-12-20 18:14:36 +0300

    fix

--- Assets/Scripts/Weapons/AimWeaponEvent.cs
+++ Assets/Scripts/Weapons/AimWeaponEvent.cs
@@ -1,18 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Vector2 = System.Numerics.Vector2;
 
+
+[DisallowMultipleComponent]
 public class AimWeaponEvent : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public event Action<AimWeaponEvent, AimWeaponEventArgs> OnWeaponAim;
 
-    // Update is called once per frame
-    void Update()
+    public void CallAimWeaponEvent(AimDirection aimDirection, float aimAngle, float weaponAimAngle ,Vector3 weaponAimDirectionVector)
     {
-        
+        OnWeaponAim?.Invoke(this, new AimWeaponEventArgs()
+        {
+            aimDirection = aimDirection, aimAngle = aimAngle, weaponAimAngle = weaponAimAngle,
+            weaponAimDirectionVector = weaponAimDirectionVector
+        });
     }
+    
+}
+
+
+public class AimWeaponEventArgs : EventArgs
+{
+    public AimDirection aimDirection;
+    public float aimAngle;
+    public float weaponAimAngle;
+    public Vector3 weaponAimDirectionVector;
+    
 }
